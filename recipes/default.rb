d2113b13a8cdb88c62dd604ee0041e67ac6cc085
okisanjp <okisan.jp@gmail.com>
2014-11-06 12:41:40 +0900

    .

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,14 +10,14 @@
 git "/home/#{node['current_user']}" do
   repository "https://github.com/okisanjp/dotfiles.git"
   revision "master"
-  user #{node['current_user']}
-  group #{node['current_user']}
+  user node['current_user']
+  group node['current_user']
   action :sync
 end
 
 execute "action" do
   command <<-EOH
-    su - #{'node[current_user']}
+    su - #{node['current_user']}
     cd ~/dotfiles
     sh ./install.sh
     exit
